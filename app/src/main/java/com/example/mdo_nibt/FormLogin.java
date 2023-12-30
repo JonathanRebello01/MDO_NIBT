c9aac88e9de270426751e794413a80eb12c73814
JonathanRebello01 <rebello.jonathan@gmail.com>
2023-12-29 23:45:13 -0300

    Adiciona autenticação de usuário com FIrebaseAuth

--- app/src/main/java/com/example/mdo_nibt/FormLogin.java
+++ app/src/main/java/com/example/mdo_nibt/FormLogin.java
@@ -2,16 +2,35 @@ package com.example.mdo_nibt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
     private TextView text_tela_cadastro;
     private Button bt_login;
+    private EditText edt_email, edt_senha;
+    private ProgressBar progressBar;
+    private ImageView visualizar_senha;
+    boolean logicamaster = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,8 +58,15 @@ public class FormLogin extends AppCompatActivity {
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
-                startActivity(intent);
+                String email = edt_email.getText().toString();
+                String senha = edt_senha.getText().toString();
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, "Preencha TODOS os campos ; )", Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                }
+                else {
+                    autenticarUsuario(v);
+                }
             }
         });
 
@@ -53,12 +79,63 @@ public class FormLogin extends AppCompatActivity {
 
     }
 
+    private void autenticarUsuario(View v){
+
+        String email = edt_email.getText().toString();
+        String senha = edt_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            telaPrincipal();
+                        }
+                    },500);
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Snackbar snackbar = Snackbar.make(v, "Erro na tentativa de login" + e, Snackbar.LENGTH_SHORT);
+                snackbar.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+        if(usuarioAtual != null){
+            telaPrincipal();
+        }
+    }
+
+    private void telaPrincipal(){
+
+        Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
+        startActivity(intent);
+
+    }
+
     private void IniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
-        bt_login = findViewById(R.id.bt_entrar);
-    }
-    private void avancarPginaTeste(){
 
+        bt_login = findViewById(R.id.bt_entrar);
+
+        progressBar = findViewById(R.id.login_progressbar);
+
+        edt_email = findViewById(R.id.edit_email_login);
+        edt_senha = findViewById(R.id.edt_senha_login);
+
+        visualizar_senha = findViewById(R.id.visualizar_senha_login);
     }
 
 }
