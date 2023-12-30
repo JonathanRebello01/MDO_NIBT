7e2c27f73e8beda8c5d89d9f1514bd585ac11199
JonathanRebello01 <rebello.jonathan@gmail.com>
2023-12-29 23:41:54 -0300

    Adiciona 'activity_historico.xml' e 'Historico.java'

---
+++ app/src/main/java/com/example/mdo_nibt/Historico.java
@@ -0,0 +1,16 @@
+package com.example.mdo_nibt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Historico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico);
+        getSupportActionBar().hide();
+
+    }
+}
