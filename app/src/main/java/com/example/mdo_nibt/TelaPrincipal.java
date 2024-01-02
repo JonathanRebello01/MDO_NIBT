9be052df3516d925ad2f9728089003e4670bd387
JonathanRebello01 <rebello.jonathan@gmail.com>
2024-01-01 21:13:48 -0300

    Correção do bug de não aparecer o nome na tela principal

--- app/src/main/java/com/example/mdo_nibt/TelaPrincipal.java
+++ app/src/main/java/com/example/mdo_nibt/TelaPrincipal.java
@@ -8,6 +8,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -111,7 +112,7 @@ public class TelaPrincipal extends AppCompatActivity {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
                 if(documentSnapshot != null){
-                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    nomeUsuario.setText(documentSnapshot.getString("Nome"));
                     emailUsuario.setText(email);
                 }
             }
